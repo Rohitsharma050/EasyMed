1b9cf178f56d5fa73e961bf779554ebb613b5584
Rohitsharma050 <149524501+Rohitsharma050@users.noreply.github.com>
2025-11-08 01:53:07 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server :{port:5173}
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+    historyApiFallback: true
+  },
+  preview: {
+    port: 8080,
+    historyApiFallback: true
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
